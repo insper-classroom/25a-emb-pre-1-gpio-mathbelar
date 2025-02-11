d4ca87fd0620e053ae42648e1e8e26176479da71
Belarminoino <matheuspb@al.insper.edu.br>
2025-02-11 14:44:45 -0300

    run1.2

--- exe1/main.c
+++ exe1/main.c
@@ -1,25 +1,29 @@
-#include "hardware/gpio.h"
-#include "pico/stdlib.h"
 #include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
 
-const int LED_PIN_R = 5;
-const int LED_PIN_L = 5;
+const int LED_PIN = 5;
+const int LED_PIN2 = 28;
 
 
 int main() {
-  stdio_init_all();
+    stdio_init_all();
 
-  gpio_init(LED_PIN_R);
-  gpio_init(LED_PIN_L);
-  gpio_set_dir(LED_PIN_R, GPIO_OUT);
-  gpio_set_dir(LED_PIN_L, GPIO_OUT);
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    gpio_init(LED_PIN2);
+    gpio_set_dir(LED_PIN2, GPIO_OUT);
 
 
-  while (true) {
-    gpio_put(LED_PIN_R, 1);
-    sleep_ms(250);
+    while (true) {
+        gpio_put(LED_PIN, 1);
+        gpio_put(LED_PIN2, 1);
+        sleep_ms(250);
 
-    gpio_put(LED_PIN_L, 0);
-    sleep_ms(250);
-  }
+        gpio_put(LED_PIN, 0);
+        gpio_put(LED_PIN2, 0);
+        sleep_ms(250);
+    }
+
 }
