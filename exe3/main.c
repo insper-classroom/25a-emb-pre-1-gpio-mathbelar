f5a618ef45d474f6a5d4b780c42b36ce349eb38a
Belarminoino <matheuspb@al.insper.edu.br>
2025-02-13 09:13:16 -0300

    todos_ex3

--- exe3/main.c
+++ exe3/main.c
@@ -8,8 +8,13 @@ const int botao_verde = 26;
 const int led_vermelho = 4;
 const int led_verde = 6;
 
+
+
 int main() {
   stdio_init_all();
+
+  int status_vermelho = 0;
+  int status_verde = 0;
 
   gpio_init(botao_vermelho);
   gpio_set_dir(botao_vermelho, GPIO_IN);
@@ -28,26 +33,65 @@ int main() {
   gpio_set_dir(led_verde, GPIO_OUT);
 
 
+  // while(true){
+
+  //   // jeito que o corsi me mostrou mo daora
+  //   if(!gpio_get(botao_vermelho) ){
+  //     status_vermelho = !status_vermelho;  //sacada boa
+  //     gpio_put(led_vermelho, status_vermelho);
+  //     while (!gpio_get(botao_vermelho)) {
+  //     };
+
+  //   }
+
+  // }
+
   while (true) {
-    if(!gpio_get(botao_vermelho)){
+
+    // esse if verifica se apertou o botao e o esta desligado o led
+    if(!gpio_get(botao_vermelho) && status_vermelho == 0){
 
       gpio_put(led_vermelho,1);
-      sleep_ms(250);
+      status_vermelho = 1;
+      
+      // while para soltar o botao
+      while (!gpio_get(botao_vermelho)) {
+      };
+
+        
+    }
+
+    // if para caso o led esta ligado e quando vc aperta ele desliga o led
+    if (!gpio_get(botao_vermelho) && status_vermelho == 1){
 
       gpio_put(led_vermelho,0);
+      status_vermelho = 0;
+      }
+
       while (!gpio_get(botao_vermelho)) {
       };
-    }
-    if(!gpio_get(botao_verde)){
+
+
+      //verde
+    if(!gpio_get(botao_verde) && status_verde == 0){
 
       gpio_put(led_verde,1);
-      sleep_ms(250);
+      status_verde = 1;
 
-      gpio_put(led_verde,0);
-      while (!gpio_get(botao_vermelho)) {
+
+      while (!gpio_get(botao_verde)) {
       };
 
     }
+
+    if (!gpio_get(botao_verde) && status_verde == 1){
+
+      gpio_put(led_verde,0);
+      status_verde = 0;
+      }
+
+      while (!gpio_get(botao_verde)) {
+      };
 
 
   }
